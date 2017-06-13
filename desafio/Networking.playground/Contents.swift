6950607b3765ad720564d71b6ca61be6d8865ee0
Fabio Nogueira de Almeida <fna.contact@gmail.com>
2017-06-12 22:59:28 -0300

    Add playground

---
+++ desafio/Networking.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Resource {
+//    let url: NSURL
+//    let parse: NSData -> A?
+}
+
+final class WebService {
+    
+    func load<A>(resource: String, completion: (A?) -> ()) {
+        
+    }
+    
+}
